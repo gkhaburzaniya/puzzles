722ee1479ef33f60b384c9ab1921c99c61108a1a
George Khaburzaniya <Dobleman@gmail.com>
2013-03-31 00:22:31 -0700

    Updated Test Problems and add gitignore

--- test_problems.py
+++ test_problems.py
@@ -21,6 +21,35 @@ class EulerTest(TestCase):
     def test_problem4(self):
         verify_problem(4, 906609)
 
+    def test_problem5(self):
+        verify_problem(5, 232792560)
+
+    def test_problem6(self):
+        verify_problem(6, 25164150)
+
+    def test_problem7(self):
+        verify_problem(7, 104743)
+
+    def test_problem8(self):
+        verify_problem(8, 40824)
+
+    def test_problem9(self):
+        verify_problem(9, 31875000)
+
+    def test_problem10(self):
+        verify_problem(10, 142913828922)
+
+    def test_problem11(self):
+        verify_problem(11, 70600674)
+
+    def test_problem12(self):
+        verify_problem(12, 76576500)
+
+    def test_problem13(self):
+        verify_problem(13, 5537376230)
+
+    def test_problem14(self):
+        verify_problem(14, 837799)
 
 if __name__ == "__main__":
     main()
