5d87ccd4989400c6e36ea8f572dca5daf847addb
Trey Hunner <trey@treyhunner.com>
2013-03-30 23:49:40 -0700

    Simplify tests

--- test_problems.py
+++ test_problems.py
@@ -1,23 +1,25 @@
 from unittest import TestCase, main
 
 
+def verify_problem(num, expected_answer):
+    module = __import__("Euler{0}".format(num))
+    assert module.answer == expected_answer, "{0} != {1}".format(
+        module.answer, expected_answer)
+
+
 class EulerTest(TestCase):
 
     def test_problem1(self):
-        from Euler1 import answer
-        self.assertEqual(answer, 233168)
+        verify_problem(1, 233168)
 
     def test_problem2(self):
-        from Euler2 import answer
-        self.assertEqual(answer, 4613732)
+        verify_problem(2, 4613732)
 
     def test_problem3(self):
-        from Euler3 import answer
-        self.assertEqual(answer, 6857)
+        verify_problem(3, 6857)
 
     def test_problem4(self):
-        from Euler4 import answer
-        self.assertEqual(answer, 906609)
+        verify_problem(4, 906609)
 
 
 if __name__ == "__main__":
