744297af2393a833352cb88c23926b2253650b62
George Khaburzaniya <Dobleman@gmail.com>
2013-04-07 01:42:20 -0700

    Finishing up for the night

--- Euler1.py
+++ Euler1.py
@@ -1,5 +1,3 @@
-target, divisor1, divisor2 = 1000, 3, 5
-
-answer = sum(i for i in range(target)
-             if i % divisor1 == 0 or i % divisor2 == 0)
+target, div1, div2 = 1000, 3, 5
+answer = sum(i for i in range(target) if i % div1 == 0 or i % div2 == 0)
 print(answer)
