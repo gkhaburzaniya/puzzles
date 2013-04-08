21859ea92dc4e74e2f3330bf79296890b7d43aae
George Khaburzaniya <Dobleman@gmail.com>
2013-04-07 21:53:35 -0700

    Some Formatting and tests

--- euler3.py
+++ euler3.py
@@ -1,6 +1,6 @@
-target = 600851475143
+from primes import is_prime, low_prime
 
-from Primes import is_prime, low_prime
+target = 600851475143
 while True:
     if is_prime(target):
         answer = target
