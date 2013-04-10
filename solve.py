baa02b3178744aebd38fdf07e16656ccef082c64
George Khaburzaniya <Dobleman@gmail.com>
2013-04-09 17:04:47 -0700

    Ignore some more stuff

--- solve.py
+++ solve.py
@@ -6,5 +6,5 @@ while i[0] != 'q' and i[0] != 'Q' and i[0] != 'e' and i[0] != 'E':
     print(timeit(
         'from euler' + i +
         ' import answer; print("\\nAnswer\\n" + str(answer) + "\\n\\nTime")',
-        number=1), '\n')
-    i = input('Solve which problem?\n')
+        number=1))
+    i = input('\nSolve which problem?\n')
