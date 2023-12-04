e1decc7b93383560afa7754f5623051b4026bc1a
George Khaburzaniya <george.khaburzaniya@gmail.com>
2023-12-03 21:25:27 -0800

    Solve 2023-4 AoC.

---
+++ advent_of_code/2023/day_4.py
@@ -0,0 +1,21 @@
+puzzle_input = open("day_4_input.txt")
+
+total = 0
+
+for line in puzzle_input:
+    winning_numbers = []
+    your_numbers = False
+    score = 0
+    for value in line.split()[2:]:
+        if value != "|" and not your_numbers:
+            winning_numbers.append(int(value))
+        elif value == "|":
+            your_numbers = True
+        elif your_numbers and int(value) in winning_numbers:
+            if score == 0:
+                score = 1
+            else:
+                score *= 2
+    total += score
+
+print(total)
---
+++ advent_of_code/2023/day_4_part_2.py
@@ -0,0 +1,18 @@
+puzzle_input = open("day_4_input.txt").readlines()
+
+instances = [1] * len(puzzle_input)
+for i, line in enumerate(puzzle_input):
+    winning_numbers = []
+    your_numbers = False
+    matches = 0
+    for value in line.split()[2:]:
+        if value != "|" and not your_numbers:
+            winning_numbers.append(int(value))
+        elif value == "|":
+            your_numbers = True
+        elif your_numbers and int(value) in winning_numbers:
+            matches += 1
+    for j in range(matches):
+        instances[i+1+j] += instances[i]
+
+print(sum(instances))
