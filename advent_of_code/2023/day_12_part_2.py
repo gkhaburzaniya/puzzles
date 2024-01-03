b9931c35be7952bf0d6dfaef186043f80503f80d
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-01-03 13:30:20 -0800

    Bugged but maybe faster?

--- advent_of_code/2023/day_12_part_2.py
+++ advent_of_code/2023/day_12_part_2.py
@@ -6,23 +6,45 @@ from dataclasses import dataclass
 puzzle_input = open("inputs/day_12_input.txt")
 
 
-damaged = "#"
-operational = "."
-unknown = "?"
+DAMAGED = "#"
+OPERATIONAL = "."
+UNKNOWN = "?"
 
 
-def find_ways(pattern, springs, unknown_damageds, start_i=0):
+def find_ways(pattern, springs, unknown_damageds, start_i=0, filled_records=0):
     ways = 0
+    record = records[filled_records]
     for i, spring in enumerate(springs[start_i:]):
-        if spring == unknown:
-            test_springs = (springs[:i + start_i] + damaged
-                            + springs[i + start_i + 1:])
+        if spring == UNKNOWN:
+            test_springs = (
+                        springs[:start_i + i] + DAMAGED * record + OPERATIONAL
+                        + springs[start_i + i + record + 1:])
             if re.fullmatch(pattern, test_springs):
-                if unknown_damageds == 1:
+                if unknown_damageds == record:
                     ways += 1
                 else:
-                    ways += find_ways(pattern, test_springs,
-                                      unknown_damageds - 1, i + start_i + 1)
+                    filled_records += 1
+                    ways += find_ways(pattern,
+                                      test_springs,
+                                      unknown_damageds - record,
+                                      i + start_i + record + 1,
+                                      filled_records)
+        if spring == DAMAGED:
+            new_re = ""
+            for record in records[filled_records + 1:]:
+                new_re += r"(?:#|\?){" + str(record) + r"}(?:\.|\?)+"
+            if not re.fullmatch(r"(?:#|\?){" + str(record) + r"}(?:\.|\?)+",
+                                springs[start_i + 1:]):
+                break
+            test_springs = (
+                    springs[:i + start_i] + DAMAGED * record + OPERATIONAL
+                    + springs[i + start_i + record + 1:])
+            filled_records += 1
+            ways += find_ways(pattern,
+                              test_springs,
+                              unknown_damageds,
+                              i + start_i + record + 1,
+                              filled_records)
     return ways
 
 
