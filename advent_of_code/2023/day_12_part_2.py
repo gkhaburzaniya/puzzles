a8042f7e4ceed148450977fa78aa51b33a2fe54e
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-01-04 08:49:36 -0800

    Fix the million bugs.

--- advent_of_code/2023/day_12_part_2.py
+++ advent_of_code/2023/day_12_part_2.py
@@ -11,40 +11,46 @@ OPERATIONAL = "."
 UNKNOWN = "?"
 
 
-def find_ways(pattern, springs, unknown_damageds, start_i=0, filled_records=0):
+def find_ways(pattern, springs, start_i=0, filled_records=0):
     ways = 0
-    record = records[filled_records]
+    curr_record = records[filled_records]
     for i, spring in enumerate(springs[start_i:]):
         if spring == UNKNOWN:
+            if springs[start_i + i + curr_record] == DAMAGED:
+                continue
             test_springs = (
-                        springs[:start_i + i] + DAMAGED * record + OPERATIONAL
-                        + springs[start_i + i + record + 1:])
+                    springs[:start_i + i] + DAMAGED * curr_record +
+                    OPERATIONAL + springs[start_i + i + curr_record + 1:]
+            )
             if re.fullmatch(pattern, test_springs):
-                if unknown_damageds == record:
+                if len(re.findall(DAMAGED, test_springs)) == sum(records):
                     ways += 1
                 else:
-                    filled_records += 1
                     ways += find_ways(pattern,
                                       test_springs,
-                                      unknown_damageds - record,
-                                      i + start_i + record + 1,
-                                      filled_records)
-        if spring == DAMAGED:
+                                      i + start_i + curr_record + 1,
+                                      filled_records + 1)
+            if (UNKNOWN not in test_springs[start_i + i:] and
+                    springs[start_i + i + curr_record] != UNKNOWN):
+                break
+        elif spring == DAMAGED:
             new_re = ""
-            for record in records[filled_records + 1:]:
+            for record in records[filled_records:]:
                 new_re += r"(?:#|\?){" + str(record) + r"}(?:\.|\?)+"
-            if not re.fullmatch(r"(?:#|\?){" + str(record) + r"}(?:\.|\?)+",
-                                springs[start_i + 1:]):
+            if not re.fullmatch(new_re, springs[start_i + i:]):
                 break
             test_springs = (
-                    springs[:i + start_i] + DAMAGED * record + OPERATIONAL
-                    + springs[i + start_i + record + 1:])
-            filled_records += 1
+                    springs[:i + start_i] + DAMAGED * curr_record + OPERATIONAL
+                    + springs[i + start_i + curr_record + 1:])
+            if len(re.findall(DAMAGED, test_springs)) == sum(records):
+                ways += 1
+            if (UNKNOWN not in test_springs[start_i + i:] and
+                    springs[start_i + i + curr_record] != UNKNOWN):
+                break
             ways += find_ways(pattern,
                               test_springs,
-                              unknown_damageds,
-                              i + start_i + record + 1,
-                              filled_records)
+                              i + start_i + curr_record + 1,
+                              filled_records + 1)
     return ways
 
 
@@ -61,7 +67,7 @@ for line in puzzle_input:
     re_pattern = r"(?:\.|\?)*"
     for record in records:
         re_pattern += r"(?:#|\?){" + str(record) + r"}(?:\.|\?)+"
-    ways += find_ways(re_pattern, springs, unknown_damageds)
+    ways += find_ways(re_pattern, springs)
 
 
 print(ways)
