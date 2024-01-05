ba9d23465a854b302bd360f6fd3e945362fab05a
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-01-05 08:11:37 -0800

    Tiny bit more refactoring.

--- advent_of_code/2023/day_12_part_2.py
+++ advent_of_code/2023/day_12_part_2.py
@@ -9,7 +9,7 @@ puzzle_input = open("inputs/day_12_input.txt")
 DAMAGED = "#"
 OPERATIONAL = "."
 UNKNOWN = "?"
-MULTIPLIER = 1
+MULTIPLIER = 4
 
 
 def find_ways(curr_springs, filled_records=0):
@@ -18,6 +18,7 @@ def find_ways(curr_springs, filled_records=0):
         return curr_ways
     curr_record = records[filled_records]
     pattern = patterns[filled_records]
+    curr_sum = sums[filled_records]
     for i, spring in enumerate(curr_springs):
         if not re.fullmatch(pattern, curr_springs[i:]):
             break
@@ -31,23 +32,17 @@ def find_ways(curr_springs, filled_records=0):
                     OPERATIONAL + curr_springs[i + curr_record + 1:]
             )
             if re.fullmatch(pattern, test_springs):
-                if len(re.findall(DAMAGED, test_springs)) == sum(records[filled_records:]):
+                if len(re.findall(DAMAGED, test_springs)) == curr_sum:
                     curr_ways += 1
                 else:
                     curr_ways += find_ways(test_springs[i+curr_record+1:],
                                            filled_records + 1)
-            if (UNKNOWN not in test_springs[i:] and
-                    curr_springs[i + curr_record] != UNKNOWN):
-                break
         elif spring == DAMAGED:
             test_springs = (
                     curr_springs[:i] + DAMAGED * curr_record + OPERATIONAL
                     + curr_springs[i + curr_record + 1:])
-            if len(re.findall(DAMAGED, test_springs)) == sum(records[filled_records:]):
+            if len(re.findall(DAMAGED, test_springs)) == curr_sum:
                 curr_ways += 1
-            if (UNKNOWN not in test_springs[i:] and
-                    curr_springs[i + curr_record] != UNKNOWN):
-                break
             curr_ways += find_ways(test_springs[i+curr_record+1:],
                                    filled_records + 1)
     return curr_ways
@@ -61,10 +56,13 @@ for line in puzzle_input:
     springs = words[0]
     records = [int(record) for record in words[1].split(',')]
     patterns = []
+    sums = []
     for i in range(len(records)):
         re_pattern = r"(?:\.|\?)*"
         for record in records[i:]:
             re_pattern += r"(?:#|\?){" + str(record) + r"}(?:\.|\?)+"
         patterns.append(re_pattern)
+        sums.append(sum(records[i:]))
+
     ways += find_ways(springs)
     print(line, ways)
