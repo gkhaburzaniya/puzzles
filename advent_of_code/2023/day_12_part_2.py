6ac82425efda446b42da02ef5f201436e3176679
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-01-10 15:58:19 -0800

    Not quite working still.

--- advent_of_code/2023/day_12_part_2.py
+++ advent_of_code/2023/day_12_part_2.py
@@ -25,46 +25,64 @@ def find_ways(curr_spans, filled_records=0):
                 damaged_spans_end = damaged_spans[-1].end()
                 damage_len = damaged_spans_end - damaged_spans_start
                 excess_len = curr_record - damage_len
-                curr_ways = 1 + 2 * (len(span) - damage_len - excess_len)
+                curr_ways = 1 + min(len(span) - damage_len, 2 * excess_len)
                 break
             if len(span) >= curr_record:
                 curr_ways += len(span) - curr_record + 1
     else:
-        curr_springs = ".".join(curr_spans) + "."
-        pattern = patterns[filled_records]
         for i, span in enumerate(curr_spans):
-            if not pattern.fullmatch(curr_springs[i:]):
-                break
-            if DAMAGED in curr_springs[:i]:
-                break
-            if curr_springs[i + curr_record] == DAMAGED:
-                continue
-            if len(span-i) >= curr_record:
-                for j, char in enumerate(span):
-                    if len(span) > curr_record + records[filled_records + 1]:
-                        new_span = span[j + curr_record + 1:]
-                        try:
-                            new_spans = [new_span] + curr_spans[i + 1:]
-                        except IndexError:
-                            new_spans = [new_span]
-                    else:
-                        new_spans = curr_spans[i + 1:]
+            damaged_spans = list(re.finditer("#+", span))
+            if damaged_spans:
+                # Record fits before damage starts
+                for j in range(damaged_spans[0].start() - (curr_record + 1)):
+                    new_span = span[j+curr_record+1:]
+                    new_spans = [new_span]
+                    if len(curr_spans) > i + 1:
+                        new_spans += curr_spans[i+1:]
+                    if others_fit(new_spans, filled_records + 1):
+                        curr_ways += find_ways(new_spans, filled_records + 1)
+                # Damage starts
+                for dam_span in damaged_spans:
+                    if len(span) - dam_span.start() < curr_record:
+                        return curr_ways
+                    new_spans = []
+                    if dam_span.end() + 1 < len(span):
+                        new_span = span[dam_span.end() + 1]
+                        new_spans += [new_span]
+                    if len(curr_spans) > i + 1:
+                        new_spans += curr_spans[i + 1:]
+                    if others_fit(new_spans, filled_records + 1):
+                        curr_ways += find_ways(new_spans,
+                                               filled_records + 1)
+                return curr_ways
+            for j in range(len(span) - curr_record + 1):
+                new_spans = []
+                if len(span) > j + curr_record + 1:
+                    new_span = span[j+curr_record+1]
+                    new_spans += [new_span]
+                if len(curr_spans) > i + 1:
+                    new_spans += curr_spans[i + 1:]
+                if others_fit(new_spans, filled_records + 1):
                     curr_ways += find_ways(new_spans, filled_records + 1)
+                else:
+                    break
+
     return curr_ways
 
 
-# def fit_in_damage(curr_spans, filled_records=0):
-#     curr_record = records[filled_records]
-#     if filled_records == len(records) - 1:
-#         for span in curr_spans:
-#             damaged_in_span = re.search(DAMAGED, span)
-#             if damaged_in_span:
+def others_fit(curr_spans, filled_records):
+    curr_springs = ".".join(curr_spans) + "."
+    pattern = patterns[filled_records]
+    if re.fullmatch(pattern, curr_springs):
+        return True
+    else:
+        return False
 
 
 ways = 0
 for line in puzzle_input:
     words = line.split()
-    words[0] = (words[0] + "?") * MULTIPLIER + words[0] + "."
+    words[0] = (words[0] + "?") * MULTIPLIER + words[0]
     words[1] = (words[1] + ",") * MULTIPLIER + words[1]
     springs = words[0]
     records = [int(record) for record in words[1].split(',')]
@@ -76,8 +94,7 @@ for line in puzzle_input:
         patterns.append(re.compile(re_pattern))
     spans = re.findall(r"[?#]+", springs)
     line_ways = find_ways(spans)
-    print(line, line_ways)
-    print(spans)
+    print(spans, line_ways)
     ways += line_ways
 
 print(ways)
