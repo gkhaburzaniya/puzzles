1cb5f26788b54aae1b1e099966a0b2cc6fa552cb
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-04-11 09:53:04 -0700

    Solve with help from reddit.

--- advent_of_code/2023/day_12_part_2.py
+++ advent_of_code/2023/day_12_part_2.py
@@ -9,58 +9,51 @@ puzzle_input = open("inputs/day_12_input.txt")
 DAMAGED = "#"
 OPERATIONAL = "."
 UNKNOWN = "?"
-MULTIPLIER = 1
+MULTIPLIER = 4
+PATTERN = re.compile(r"#+\.")
 
 
 @cache
-def find_ways(curr_springs, filled_records=0):
-    curr_ways = 0
-    if filled_records == len(records):
-        return curr_ways
-    curr_record = records[filled_records]
-    pattern = patterns[filled_records]
-    curr_sum = sums[filled_records]
-    records_left = tuple(records[filled_records:])
-    for i, spring in enumerate(curr_springs):
-        if not pattern.fullmatch(curr_springs[i:]):
-            break
-        if DAMAGED in curr_springs[:i]:
-            break
-        if curr_springs[i + curr_record] == DAMAGED:
-            continue
-        if spring == UNKNOWN or spring == DAMAGED:
-            test_springs = (
-                    DAMAGED * curr_record +
-                    OPERATIONAL + curr_springs[i + curr_record + 1:]
-            )
-            if pattern.fullmatch(test_springs):
-                if len(re.findall(DAMAGED, test_springs)) == curr_sum:
-                    new_ways = 1
-                else:
-                    new_ways = find_ways(test_springs[curr_record+1:],
-                                         filled_records + 1)
-                curr_ways += new_ways
-    return curr_ways
+def find_ways(springs, records):
+    ways = 0
+    if UNKNOWN not in springs:
+        records_filled = [len(damaged_springs) - 1
+                          for damaged_springs in PATTERN.findall(springs)]
+        if tuple(records_filled) == records:
+            ways += 1
+        return ways
+    first_unknown = springs.index(UNKNOWN)
+    records_filled = [len(damaged_springs) - 1
+                      for damaged_springs
+                      in PATTERN.findall(springs[:first_unknown])]
+    if len(records_filled) > len(records):
+        return 0
+    for i, record in enumerate(records_filled):
+        if record != records[i]:
+            return 0
+    records = records[len(records_filled):]
+    start_string = ""
+    last_damaged = re.search(r"#+\?", springs[:first_unknown + 1])
+    if last_damaged:
+        start_string = springs[last_damaged.start():first_unknown]
+    ways += find_ways(
+        start_string + OPERATIONAL + springs[first_unknown + 1:], records)
+    ways += find_ways(
+        start_string + DAMAGED + springs[first_unknown + 1:], records)
+
+    return ways
 
 
-ways = 0
+total_ways = 0
 for line in puzzle_input:
     words = line.split()
     words[0] = (words[0] + "?") * MULTIPLIER + words[0] + "."
     words[1] = (words[1] + ",") * MULTIPLIER + words[1]
-    springs = words[0]
-    records = [int(record) for record in words[1].split(',')]
-    patterns = []
-    sums = []
-    for i in range(len(records)):
-        re_pattern = r"[?.]*"
-        for record in records[i:]:
-            re_pattern += r"[?#]{" + str(record) + r"}[?.]+"
-        patterns.append(re.compile(re_pattern))
-        sums.append(sum(records[i:]))
+    line_springs = words[0]
+    line_records = [int(record) for record in words[1].split(',')]
 
-    line_ways = find_ways(springs)
-    ways += line_ways
+    line_ways = find_ways(line_springs, tuple(line_records))
+    total_ways += line_ways
 
-print(ways)
+print(total_ways)
 print(time.time() - start)
