345fdcf4e7d6f61115be4962b35db3e8ee138233
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-04-13 15:52:54 -0700

    Solve 2023-13-2 AoC.

---
+++ advent_of_code/2023/day_13_part_2.py
@@ -0,0 +1,54 @@
+from Levenshtein import distance
+
+
+puzzle_input = open("inputs/day_13_input.txt")
+
+
+def solve(field):
+    before_mirror = find_mirror(field)
+    if not before_mirror:
+        field = flip(field)
+        before_mirror = find_mirror(field)
+    else:
+        before_mirror *= 100
+    return before_mirror
+
+
+def flip(field):
+    new_field = [""] * len(field[0])
+    for row in field:
+        for i, char in enumerate(row):
+            new_field[i] += char
+    return new_field
+
+
+def find_mirror(field):
+    for i in range(len(field) - 1):
+        smudge_found = False
+        for j in range(min(i + 1, len(field) - i - 1)):
+            comparison_row = field[i + j + 1]
+            mirrored_row = field[i - j]
+            if field[i + j + 1] != field[i - j]:
+                if (
+                        not smudge_found
+                        and distance(comparison_row, mirrored_row) == 1
+                ):
+                    smudge_found = True
+                    continue
+                break
+        else:
+            if smudge_found:
+                return i + 1
+
+
+answer = 0
+puzzle = []
+
+for line in puzzle_input:
+    if line == "\n":
+        answer += solve(puzzle)
+        puzzle = []
+    else:
+        puzzle.append(line.strip())
+answer += solve(puzzle)
+print(answer)
