7987ee07a4fd57f0d839696e62a5172eb6c15ba9
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-04-15 08:51:43 -0700

    Refactor day_1 of AoC to have both answers.

--- advent_of_code/2023/day_1.py
+++ advent_of_code/2023/day_1.py
@@ -2,27 +2,30 @@ import math
 
 puzzle_input = open("inputs/day_1_input.txt")
 
-digits = ["one", "1", "two", "2", "three", "3", "four", "4", "five", "5",
-          "six", "6", "seven", "7", "eight", "8", "nine", "9"]
+DIGITS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+DIGITS_2 = ["one", "1", "two", "2", "three", "3", "four", "4", "five", "5",
+            "six", "6", "seven", "7", "eight", "8", "nine", "9"]
 
 translate = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
              "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
 total = 0
+total_2 = 0
 
 
-for line in puzzle_input:
+def solve(digits, problem):
     first_digit = "0"
     second_digit = "0"
     first_digit_pos = math.inf
     second_digit_pos = math.inf
     for digit in digits:
-        current_pos = line.find(digit)
+        current_pos = problem.find(digit)
         if current_pos != -1 and current_pos < first_digit_pos:
             first_digit = digit
             first_digit_pos = current_pos
 
-        current_pos = line[::-1].find(digit[::-1])
+        current_pos = problem[::-1].find(digit[::-1])
         if current_pos != -1 and current_pos < second_digit_pos:
             second_digit = digit
             second_digit_pos = current_pos
@@ -30,7 +33,12 @@ for line in puzzle_input:
         first_digit = translate[first_digit]
     if not second_digit.isdigit():
         second_digit = translate[second_digit]
-    total += int(first_digit + second_digit)
+    return int(first_digit + second_digit)
 
 
-print(total)
+for line in puzzle_input:
+    total += solve(DIGITS, line)
+    total_2 += solve(DIGITS_2, line)
+
+
+print(total, total_2)
