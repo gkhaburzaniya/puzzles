2bfce72d0b7c1b377d072eeb3ab36b2e9371e235
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-04-18 07:01:00 -0700

    Refactor day 4 for consistency.

--- advent_of_code/2023/day_4.py
+++ advent_of_code/2023/day_4.py
@@ -1,21 +1,28 @@
-puzzle_input = open("inputs/day_4.txt")
+puzzle_input = open("inputs/day_4.txt").readlines()
 
 total = 0
 
-for line in puzzle_input:
+instances = [1] * len(puzzle_input)
+for i, line in enumerate(puzzle_input):
     winning_numbers = []
     your_numbers = False
     score = 0
+    matches = 0
     for value in line.split()[2:]:
         if value != "|" and not your_numbers:
             winning_numbers.append(int(value))
         elif value == "|":
             your_numbers = True
         elif your_numbers and int(value) in winning_numbers:
+            matches += 1
             if score == 0:
                 score = 1
             else:
                 score *= 2
     total += score
+    for j in range(matches):
+        instances[i+1+j] += instances[i]
 
-print(total)
+total_2 = sum(instances)
+
+print(total, total_2)
