67cf71392f5472d70518c907625f485efa80df96
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-04-21 08:07:08 -0700

    Keep trying to make day_5 faster.

--- advent_of_code/2023/day_5.py
+++ advent_of_code/2023/day_5.py
@@ -146,29 +146,44 @@ for j in itertools.count():
 # ranges = []
 # num = 0
 # for transform in locations_2:
-#     ranges.append((num, transform.destination_start))
+#     ranges.append((num, transform.destination_start, num))
 #     ranges.append((transform.destination_start,
-#                    transform.destination_start + transform.length))
+#                    transform.destination_start + transform.length,
+#                    transform.destination_start))
 #     num = transform.destination_start + transform.length
 #
-# new_ranges = ranges.copy()
-# for length in ranges:
+#
+# def final_ranges(ranges):
 #     for transform_type in all_transforms:
-#         new_new_ranges = []
+#         new_ranges = []
 #         for transform in transform_type:
-#             for new_length in new_ranges:
-#                 if new_length[1] < transform.destination_start:
-#                     break
-#                 if transform.destination_start <= new_length[1] < transform.destination_start + transform.length:
-#                     new_new_ranges.append((new_length[0], transform.destination_start))
-#                     new_new_ranges.append((transform.destination_start, new_length[1]))
-#                 elif transform.destination_start + transform.length <= new_length[1]:
-#                     new_new_ranges.append((new_length[0], transform.destination_start))
-#                     new_new_ranges.append((transform.destination_start, transform.destination_start + transform.length))
-#                     new_new_ranges.append((transform.destination_start + transform.length, new_length[1]))
+#             for length in ranges:
+#                 if length[1] < transform.destination_start or length[0] >= transform.destination_start + transform.length:
+#                     new_ranges.append(length)
+#                 elif transform.destination_start > length[0] and transform.destination_start + transform.length <= length[1]:
+#                     new_ranges.append((length[0], transform.destination_start, length[2]))
+#                     new_ranges.append((transform.sourse_start, transform.sourse_start + transform.length, length[2] + (transform.destination_start - length[0])))
+#                     new_ranges.append((transform.destination_start + transform.length, length[1], length[2] + (transform.destination_start - length[0]) + transform.length))
+#                 elif length[0] >= transform.destination_start and length[1] < transform.destination_start + transform.length:
+#                     new_ranges.append((transform.sourse_start + (length[0] - transform.destination_start), ))
+#                 elif transform.destination_start <= length[1] < transform.destination_start + transform.length:
+#                     new_ranges.append((length[0], transform.destination_start, length[2]))
+#                     new_ranges.append((transform.sourse_start, transform.sourse_start + transform.length, length[2] + (transform.destination_start - length[0])))
+#
 #         else:
-#             new_ranges = new_new_ranges
-#             new_ranges.sort()
-
+#             ranges = new_ranges
+#             ranges.sort()
+#     return ranges
+#
+# ranges = final_ranges(ranges)
+#
+# def check_lengths(length):
+#     for seed in seeds_2:
+#         if (
+#                 (seed[0] <= length[0] < seed[1])
+#                 or (seed[0] <= length[1] < seed[1])
+#                 or (length[0] < seed[0] and length[1] > seed[1])
+#         ):
+#             return length
 
 print(answer, answer_2)
