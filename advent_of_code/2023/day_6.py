6f5b99c80ab84aa657bb97852cac9d526ee24ba4
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-04-30 09:56:09 -0700

    Refactor day_6 for consistency.

--- advent_of_code/2023/day_6.py
+++ advent_of_code/2023/day_6.py
@@ -1,7 +1,11 @@
+from math import sqrt, floor, ceil
+
+
 puzzle_input = open("inputs/day_6.txt").readlines()
 
 times = [int(time) for time in puzzle_input[0].split() if time.isdigit()]
-records = [int(record) for record in puzzle_input[1].split() if record.isdigit()]
+records = [
+    int(record) for record in puzzle_input[1].split() if record.isdigit()]
 
 multiple = 1
 for i, time in enumerate(times):
@@ -11,4 +15,19 @@ for i, time in enumerate(times):
             ways_to_beat += 1
     multiple *= ways_to_beat
 
-print(multiple)
+
+time = ""
+record = ""
+for word in puzzle_input[0].split():
+    if word.isdigit():
+        time += word
+for word in puzzle_input[1].split():
+    if word.isdigit():
+        record += word
+time = int(time)
+record = int(record)
+
+min_to_beat = ceil((time - sqrt(time**2 - 4 * record))/2)
+max_to_beat = floor((time + sqrt(time**2 - 4 * record))/2)
+ways_to_beat = max_to_beat - min_to_beat + 1
+print(multiple, ways_to_beat)
