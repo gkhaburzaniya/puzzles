8ed39ee175a067befe0c601b9be7acec0220f1af
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-05-03 14:06:20 -0700

    Refactor day_9 for consistency.

--- advent_of_code/2023/day_9.py
+++ advent_of_code/2023/day_9.py
@@ -1,6 +1,7 @@
 puzzle_input = open("inputs/day_9.txt")
 
 total = 0
+total_2 = 0
 for line in puzzle_input:
     value_sets = [[int(word) for word in line.split()]]
     while set(value_sets[-1]) != {0}:
@@ -10,7 +11,10 @@ for line in puzzle_input:
         value_sets.append(new_values)
     value_sets.reverse()
     for i, values in enumerate(value_sets[1:]):
-        values.insert(0, (values[0]-value_sets[i][0]))
-    total += value_sets[-1][0]
+        values.append(values[-1] + value_sets[i][-1])
+        values.insert(0, (values[0] - value_sets[i][0]))
 
-print(total)
+    total += value_sets[-1][-1]
+    total_2 += value_sets[-1][0]
+
+print(total, total_2)
