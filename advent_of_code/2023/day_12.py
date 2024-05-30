a292dc93db198a54f5d21791db840021103439d2
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-05-30 07:10:20 -0700

    Make day_12 faster by solving even more dynamically.

--- advent_of_code/2023/day_12.py
+++ advent_of_code/2023/day_12.py
@@ -1,69 +1,44 @@
-import re
-import time
 from functools import cache
-start = time.time()
 
-puzzle_input = open("inputs/day_12.txt").readlines()
-
+puzzle_input = open("inputs/day_12.txt")
 
 DAMAGED = "#"
 OPERATIONAL = "."
 UNKNOWN = "?"
-PATTERN = re.compile(r"#+\.")
+OPERATIONAL_OR_UNKNOWN = OPERATIONAL + UNKNOWN
 
 
-@cache
-def find_ways(springs, records):
-    ways = 0
-    if UNKNOWN not in springs:
-        records_filled = [len(damaged_springs) - 1
-                          for damaged_springs in PATTERN.findall(springs)]
-        if tuple(records_filled) == records:
-            ways += 1
+def solve(springs, records, multiplier=1):
+    springs = springs * multiplier
+    records = records * multiplier
+
+    @cache
+    def dynamic_solve(checked_springs, filled_records, ways=0):
+        if checked_springs == len(springs):
+            return filled_records == len(records)
+
+        if springs[checked_springs] in OPERATIONAL_OR_UNKNOWN:
+            ways += dynamic_solve(checked_springs + 1, filled_records)
+
+        try:
+            next_record = checked_springs + records[filled_records]
+            if (OPERATIONAL not in springs[checked_springs:next_record]
+                    and DAMAGED not in springs[next_record]):
+                ways += dynamic_solve(next_record + 1, filled_records + 1)
+        except IndexError:
+            pass
+
         return ways
-    first_unknown = springs.index(UNKNOWN)
-    records_filled = [len(damaged_springs) - 1
-                      for damaged_springs
-                      in PATTERN.findall(springs[:first_unknown])]
-    if len(records_filled) > len(records):
-        return 0
-    for i, record in enumerate(records_filled):
-        if record != records[i]:
-            return 0
-    records = records[len(records_filled):]
-    start_string = ""
-    last_damaged = re.search(r"#+\?", springs[:first_unknown + 1])
-    if last_damaged:
-        start_string = springs[last_damaged.start():first_unknown]
-    ways += find_ways(
-        start_string + OPERATIONAL + springs[first_unknown + 1:], records)
-    ways += find_ways(
-        start_string + DAMAGED + springs[first_unknown + 1:], records)
 
-    return ways
+    return dynamic_solve(0, 0)
 
 
-answer = 0
+answer = answer_2 = 0
 for line in puzzle_input:
-    words = line.split()
-    words[0] = words[0] + "."
-    line_springs = words[0]
-    line_records = [int(record) for record in words[1].split(',')]
-
-    line_ways = find_ways(line_springs, tuple(line_records))
-    answer += line_ways
-
-
-answer_2 = 0
-for line in puzzle_input:
-    words = line.split()
-    words[0] = (words[0] + "?") * 4 + words[0] + "."
-    words[1] = (words[1] + ",") * 4 + words[1]
-    line_springs = words[0]
-    line_records = [int(record) for record in words[1].split(',')]
-
-    line_ways = find_ways(line_springs, tuple(line_records))
-    answer_2 += line_ways
+    line_springs, line_records = line.split()
+    line_springs = (line_springs + UNKNOWN)
+    line_records = [int(record) for record in line_records.split(",")]
+    answer += solve(line_springs, line_records)
+    answer_2 += solve(line_springs, line_records, 5)
 
 print(answer, answer_2)
-print(time.time() - start)
