d4bb37645385aa9ed1fa58f3aaafd8e592fc09ec
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-06-24 15:11:24 -0700

    Solve 2023-16-1 AoC.

--- advent_of_code/2023/day_16.py
+++ advent_of_code/2023/day_16.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass
-from typing import Set
+from dataclasses import dataclass, field
 
 puzzle_input = open("inputs/day_16.txt")
 
@@ -23,20 +22,52 @@ class Beam:
 @dataclass
 class Tile:
     symbol: str
-    passed_beams: Set[str] = set
+    passed_beams: set = field(default_factory=set)
 
 
-field = {(x, y): Tile(char)
-         for y, line in enumerate(puzzle_input)
-         for x, char in enumerate(line.strip())}
+puzzle = {(x, y): Tile(char)
+          for y, line in enumerate(puzzle_input)
+          for x, char in enumerate(line.strip())}
 beams = [Beam((0, 0), RIGHT)]
 
-energized_tiles = {field[0, 0]}
+energized_tiles = {(0, 0)}
 while beams:
     beam = beams.pop()
-    tile = field[beam.location]
+    try:
+        tile = puzzle[beam.location]
+    except KeyError:
+        continue
+
     if beam.direction in tile.passed_beams:
         continue
+    tile.passed_beams.add(beam.direction)
+    energized_tiles.add(beam.location)
+
+    if tile.symbol == UR_MIRROR:
+        if beam.direction == RIGHT:
+            beam.direction = UP
+        elif beam.direction == LEFT:
+            beam.direction = DOWN
+        elif beam.direction == UP:
+            beam.direction = RIGHT
+        elif beam.direction == DOWN:
+            beam.direction = LEFT
+    elif tile.symbol == UL_MIRROR:
+        if beam.direction == RIGHT:
+            beam.direction = DOWN
+        elif beam.direction == LEFT:
+            beam.direction = UP
+        elif beam.direction == UP:
+            beam.direction = LEFT
+        elif beam.direction == DOWN:
+            beam.direction = RIGHT
+    elif tile.symbol == V_SPLITTER and beam.direction in (RIGHT, LEFT):
+        beam.direction = UP
+        beams.append(Beam(beam.location, DOWN))
+    elif tile.symbol == H_SPLITTER and (beam.direction in (UP, DOWN)):
+        beam.direction = RIGHT
+        beams.append(Beam(beam.location, LEFT))
+
     if beam.direction == RIGHT:
         beam.location = (beam.location[0] + 1, beam.location[1])
     elif beam.direction == LEFT:
@@ -45,3 +76,7 @@ while beams:
         beam.location = (beam.location[0], beam.location[1] - 1)
     elif beam.direction == DOWN:
         beam.location = (beam.location[0], beam.location[1] + 1)
+
+    beams.append(beam)
+
+print(len(energized_tiles))
