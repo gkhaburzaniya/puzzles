b5cff48644f6c22ac9728e271bc9caf29ba48687
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-06-25 14:21:56 -0700

    Turn into function.

--- advent_of_code/2023/day_16.py
+++ advent_of_code/2023/day_16.py
@@ -28,55 +28,60 @@ class Tile:
 puzzle = {(x, y): Tile(char)
           for y, line in enumerate(puzzle_input)
           for x, char in enumerate(line.strip())}
-beams = [Beam((0, 0), RIGHT)]
 
-energized_tiles = {(0, 0)}
-while beams:
-    beam = beams.pop()
-    try:
-        tile = puzzle[beam.location]
-    except KeyError:
-        continue
 
-    if beam.direction in tile.passed_beams:
-        continue
-    tile.passed_beams.add(beam.direction)
-    energized_tiles.add(beam.location)
+def tiles_energized(starting_beam):
+    beams = [starting_beam]
+    energized_tiles = set()
+    while beams:
+        beam = beams.pop()
+        try:
+            tile = puzzle[beam.location]
+        except KeyError:
+            continue
 
-    if tile.symbol == UR_MIRROR:
-        if beam.direction == RIGHT:
+        if beam.direction in tile.passed_beams:
+            continue
+        tile.passed_beams.add(beam.direction)
+        energized_tiles.add(beam.location)
+
+        if tile.symbol == UR_MIRROR:
+            if beam.direction == RIGHT:
+                beam.direction = UP
+            elif beam.direction == LEFT:
+                beam.direction = DOWN
+            elif beam.direction == UP:
+                beam.direction = RIGHT
+            elif beam.direction == DOWN:
+                beam.direction = LEFT
+        elif tile.symbol == UL_MIRROR:
+            if beam.direction == RIGHT:
+                beam.direction = DOWN
+            elif beam.direction == LEFT:
+                beam.direction = UP
+            elif beam.direction == UP:
+                beam.direction = LEFT
+            elif beam.direction == DOWN:
+                beam.direction = RIGHT
+        elif tile.symbol == V_SPLITTER and beam.direction in (RIGHT, LEFT):
             beam.direction = UP
-        elif beam.direction == LEFT:
-            beam.direction = DOWN
-        elif beam.direction == UP:
+            beams.append(Beam(beam.location, DOWN))
+        elif tile.symbol == H_SPLITTER and (beam.direction in (UP, DOWN)):
             beam.direction = RIGHT
-        elif beam.direction == DOWN:
-            beam.direction = LEFT
-    elif tile.symbol == UL_MIRROR:
+            beams.append(Beam(beam.location, LEFT))
+
         if beam.direction == RIGHT:
-            beam.direction = DOWN
+            beam.location = (beam.location[0] + 1, beam.location[1])
         elif beam.direction == LEFT:
-            beam.direction = UP
+            beam.location = (beam.location[0] - 1, beam.location[1])
         elif beam.direction == UP:
-            beam.direction = LEFT
+            beam.location = (beam.location[0], beam.location[1] - 1)
         elif beam.direction == DOWN:
-            beam.direction = RIGHT
-    elif tile.symbol == V_SPLITTER and beam.direction in (RIGHT, LEFT):
-        beam.direction = UP
-        beams.append(Beam(beam.location, DOWN))
-    elif tile.symbol == H_SPLITTER and (beam.direction in (UP, DOWN)):
-        beam.direction = RIGHT
-        beams.append(Beam(beam.location, LEFT))
+            beam.location = (beam.location[0], beam.location[1] + 1)
 
-    if beam.direction == RIGHT:
-        beam.location = (beam.location[0] + 1, beam.location[1])
-    elif beam.direction == LEFT:
-        beam.location = (beam.location[0] - 1, beam.location[1])
-    elif beam.direction == UP:
-        beam.location = (beam.location[0], beam.location[1] - 1)
-    elif beam.direction == DOWN:
-        beam.location = (beam.location[0], beam.location[1] + 1)
+        beams.append(beam)
+    return len(energized_tiles)
 
-    beams.append(beam)
 
-print(len(energized_tiles))
+answer = tiles_energized(Beam((0, 0), RIGHT))
+print(answer)
