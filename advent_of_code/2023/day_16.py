d8b687d06b550b067e21153d1866de53ba94bfdd
George Khaburzaniya <george.khaburzaniya@gmail.com>
2024-06-30 12:30:50 -0700

    Prepopulate the splitters, but that's way slow.

--- advent_of_code/2023/day_16.py
+++ advent_of_code/2023/day_16.py
@@ -25,6 +25,7 @@ class Beam:
 class Tile:
     symbol: str
     passed_beams: set = field(default_factory=set)
+    energized_tiles: set = field(default_factory=set)
 
 
 max_x = len(puzzle_input[0]) - 1
@@ -40,6 +41,7 @@ def reset_tiles():
 
 
 def tiles_energized(starting_beam):
+    reset_tiles()
     beams = [starting_beam]
     energized_tiles = set()
     while beams:
@@ -73,11 +75,19 @@ def tiles_energized(starting_beam):
             elif beam.direction == DOWN:
                 beam.direction = RIGHT
         elif tile.symbol == V_SPLITTER and beam.direction in (RIGHT, LEFT):
-            beam.direction = UP
-            beams.append(Beam(beam.location, DOWN))
+            if tile.energized_tiles:
+                energized_tiles = energized_tiles | tile.energized_tiles
+                continue
+            else:
+                beam.direction = UP
+                beams.append(Beam(beam.location, DOWN))
         elif tile.symbol == H_SPLITTER and (beam.direction in (UP, DOWN)):
-            beam.direction = RIGHT
-            beams.append(Beam(beam.location, LEFT))
+            if tile.energized_tiles:
+                energized_tiles = energized_tiles | tile.energized_tiles
+                continue
+            else:
+                beam.direction = RIGHT
+                beams.append(Beam(beam.location, LEFT))
 
         if beam.direction == RIGHT:
             beam.location = (beam.location[0] + 1, beam.location[1])
@@ -89,32 +99,40 @@ def tiles_energized(starting_beam):
             beam.location = (beam.location[0], beam.location[1] + 1)
 
         beams.append(beam)
-    return len(energized_tiles)
+    return energized_tiles
 
 
-answer = tiles_energized(Beam((0, 0), RIGHT))
+for location, tile in puzzle.items():
+    if tile.symbol in V_SPLITTER:
+        upwards_beam = tiles_energized(Beam(location, UP))
+        downwards_beam = tiles_energized(Beam(location, DOWN))
+        tile.energized_tiles = upwards_beam | downwards_beam
+    if tile.symbol in H_SPLITTER:
+        rightwards_beam = tiles_energized(Beam(location, RIGHT))
+        leftwards_beam = tiles_energized(Beam(location, LEFT))
+        tile.energized_tiles = rightwards_beam | leftwards_beam
+    reset_tiles()
+
+
+answer = len(tiles_energized(Beam((0, 0), RIGHT)))
 max_energized_tiles = 0
 
 
 for x in range(max_x + 1):
-    current_tiles_energized = tiles_energized(Beam((x, 0), DOWN))
+    current_tiles_energized = len(tiles_energized(Beam((x, 0), DOWN)))
     if current_tiles_energized > max_energized_tiles:
         max_energized_tiles = current_tiles_energized
-    reset_tiles()
-    current_tiles_energized = tiles_energized(Beam((x, max_y), UP))
+    current_tiles_energized = len(tiles_energized(Beam((x, max_y), UP)))
     if current_tiles_energized > max_energized_tiles:
         max_energized_tiles = current_tiles_energized
-    reset_tiles()
 
 for y in range(max_y + 1):
-    current_tiles_energized = tiles_energized(Beam((0, y), RIGHT))
+    current_tiles_energized = len(tiles_energized(Beam((0, y), RIGHT)))
     if current_tiles_energized > max_energized_tiles:
         max_energized_tiles = current_tiles_energized
-    reset_tiles()
-    current_tiles_energized = tiles_energized(Beam((max_x, y), LEFT))
+    current_tiles_energized = len(tiles_energized(Beam((max_x, y), LEFT)))
     if current_tiles_energized > max_energized_tiles:
         max_energized_tiles = current_tiles_energized
-    reset_tiles()
 
 answer_2 = max_energized_tiles
 
